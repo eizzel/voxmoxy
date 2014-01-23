8b07c2504f63632680fea5e59fbd288be479b099
sam perez <eizzel.perez@gmail.com>
2014-01-23 18:34:23 +0800

    Initial commit for voxmoxy

---
+++ webapp/protected/modules/default/controllers/DefaultController.php
@@ -0,0 +1,23 @@
+<?php
+
+class DefaultController extends Controller
+{
+	public function actionIndex()
+	{
+		$this->render('index');
+	}
+	
+	/**
+	 * This is the action to handle external exceptions.
+	 */
+	public function actionError()
+	{
+		if($error=Yii::app()->errorHandler->error)
+		{
+			if(Yii::app()->request->isAjaxRequest)
+				echo $error['message'];
+			else
+				$this->render('error', $error);
+		}
+	}
+}
